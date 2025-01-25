09af3d89d07dd9306840c07ff6b5c972e4e0201b
ViniciusOliveira2004 <vinicius.oliveira3@ufv.br>
2025-01-25 09:14:32 -0300

    feat: adicionando common.h

---
+++ src/common.h
@@ -0,0 +1,17 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+//Incluindo Bibliotecas padrão:
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+// Definições de cores ANSI
+#define RED "\x1b[31m"       // Vermelho
+#define WHITE "\x1b[37m"     // Branco
+#define YELLOW "\x1B[33m"    // Amarelo
+#define BLUE "\x1B[34m"      // Azul
+#define GREEN "\e[0;32m"     // Verde
+#define RESET "\033[0m"      // Resetar cor
+
+#endif
