76eadfeb66c904a51e2943711eea8bb2c379ba28
ViniciusOliveira2004 <vinicius.oliveira3@ufv.br>
2025-01-25 14:38:54 -0300

    feat: adicionando fileReader e alterações na main

---
+++ src/io/file-reader.c
@@ -0,0 +1,60 @@
+#ifndef FILE_READER_C
+#define FILE_READER_C
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+char* readFile(char* filename){
+    FILE* file = fopen(filename, "r");
+    if(!file){
+        printf("Erro ao abrir arquivo\n");
+        return NULL;
+    }
+
+    fseek(file, 0, SEEK_END);
+    long lenght = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    if(lenght == 0){
+        fclose(file);
+        printf("O arquivo fornecido está vazio\n");
+        return NULL;
+    }
+
+    char* content = (char*)malloc(lenght);
+    if(!content){
+        printf("Erro ao alocar memoria\n");
+        fclose(file);
+        return NULL;
+    }
+
+    fread(content, 1, lenght, file);
+    content[lenght]= '\0';
+
+    fclose(file);
+    return (content);
+}
+
+#endif
+
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⣤⣤⣶⣦⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀ 
+// ⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⡿⠛⠉⠙⠛⠛⠛⠛⠻⢿⣿⣷⣤⡀⠀⠀⠀⠀⠀ 
+// ⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⠋⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠈⢻⣿⣿⡄⠀⠀⠀⠀ 
+// ⠀⠀⠀⠀⠀⠀⠀⣸⣿⡏⠀⠀⠀⣠⣶⣾⣿⣿⣿⠿⠿⠿⢿⣿⣿⣿⣄⠀⠀⠀ 
+// ⠀⠀⠀⠀⠀⠀⠀⣿⣿⠁⠀⠀⢰⣿⣿⣯⠁⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣷⡄⠀ 
+// ⠀⠀⣀⣤⣴⣶⣶⣿⡟⠀⠀⠀⢸⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣷⠀ 
+// ⠀⢰⣿⡟⠋⠉⣹⣿⡇⠀⠀⠀⠘⣿⣿⣿⣿⣷⣦⣤⣤⣤⣶⣶⣶⣶⣿⣿⣿⠀ 
+// ⠀⢸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀ 
+// ⠀⣸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠉⠻⠿⣿⣿⣿⣿⡿⠿⠿⠛⢻⣿⡇⠀⠀ 
+// ⠀⣿⣿⠁⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣧⠀⠀ 
+// ⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀ 
+// ⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀ 
+// ⠀⢿⣿⡆⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀ 
+// ⠀⠸⣿⣧⡀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠃⠀⠀ 
+// ⠀⠀⠛⢿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⣰⣿⣿⣷⣶⣶⣶⣶⠶⢠⣿⣿⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⣽⣿⡏⠁⠀⠀⢸⣿⡇⠀⠀⠀ 
+// ⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⢹⣿⡆⠀⠀⠀⣸⣿⠇⠀⠀⠀ 
+// ⠀⠀⠀⠀⠀⠀⠀⢿⣿⣦⣄⣀⣠⣴⣿⣿⠁⠀⠈⠻⣿⣿⣿⣿⡿⠏⠀⠀⠀⠀ 
+// ⠀⠀⠀⠀⠀⠀⠀⠈⠛⠻⠿⠿⠿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
--- src/main.c
+++ src/main.c
@@ -2,35 +2,59 @@
 #include "algorithms/pattern-matching/brute-force/brute-force.h"
 #include "algorithms/pattern-matching/shift-and/shift-and.h"
 
-int main(int argc, char **argv)
-{ printf(" Tamanho do texto: ");
-  scanf("%ld%*[^\n]", &n);
-  getchar();
-  printf(" Tamanho do padrao: ");
-  scanf("%ld%*[^\n]", &m);
-  getchar();
-  printf(" Texto: ");
-  for (i = 1; i <= n; i++) 
-    { T[i-1] = getchar();
-      if (T[i-1] == '\n')
-      T[i-1] = ' ';
+void readText(Text T, long size, const char *message) {
+    printf("%s", message);
+    for (long i = 0; i < size; i++) {
+        T[i] = getchar();
+        if (T[i] == '\n') {
+            T[i] = ' ';
+        }
     }
-  scanf("%*[^\n]");
-  getchar();
-  printf(" Padrao: ");
-  for (i = 1; i <= m; i++) 
-    { P[i-1] = getchar();
-      if (P[i-1] == '\n')
-      P[i-1] = ' ';
+    scanf("%*[^\n]");
+    getchar();
+}
+
+void readPattern(Pattern P, long size, const char *message) {
+    printf("%s", message);
+    for ( long i = 0; i < size; i++) {
+        P[i] = getchar();
+        if (P[i] == '\n') {
+            P[i] = ' ';
+        }
     }
-  scanf("%*[^\n]");
-  getchar();
-  printf(" Forca bruta: ");
-  BruteForce(T, &n, P, &m);
-  scanf("%*[^\n]");
-  getchar();
-  printf(" Shift-And-Exato: ");
-  ShiftAnd(T, &n, P, &m);
-  scanf("%*[^\n]");
-  return 0;
+    scanf("%*[^\n]");
+    getchar();
+}
+
+int main(int argc, char **argv) {
+    Text T;
+    Pattern P;
+    long n, m;
+
+    // Ler tamanho do texto
+    printf("Tamanho do texto: ");
+    scanf("%ld%*[^\n]", &n);
+    getchar();
+
+    // Ler tamanho do padrão
+    printf("Tamanho do padrão: ");
+    scanf("%ld%*[^\n]", &m);
+    getchar();
+
+    // Ler o texto
+    readText(T, n, "Texto: ");
+
+    // Ler o padrão
+    readPattern(P, m, "Padrão: ");
+
+    // Executar os algoritmos 
+    printf("\nResultados:\n");
+
+    printf("Força bruta: ");
+    BruteForce(T, &n, P, &m);
+
+    printf("Shift-And-Exato: ");
+    ShiftAnd(T, &n, P, &m);
+
+    return 0;
 }
