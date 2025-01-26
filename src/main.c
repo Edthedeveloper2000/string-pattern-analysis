245c3a5f70a223af33d03b8e331f2eb05e566f5b
ViniciusOliveira2004 <vinicius.oliveira3@ufv.br>
2025-01-26 18:11:19 -0300

    feat: adicionando UTF-8 no projeto

--- src/main.c
+++ src/main.c
@@ -1,22 +1,7 @@
 #include "common.h"
-#include "algorithms/pattern-matching/brute-force/brute-force.h"
-#include "algorithms/pattern-matching/shift-and/shift-and.h"
-#include "io/file-reader.h"
 #include "menu/menu.h"
-
-void readPattern(Pattern P, long size, const char *message) {
-    printf("%s", message);
-    for (long i = 0; i < size; i++) {
-        P[i] = getchar();
-        if (P[i] == '\n') {
-            P[i] = ' ';
-        }
-    }
-    scanf("%*[^\n]");
-    getchar();
-}
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv) {  
+    setlocale(LC_ALL, "pt_BR.UTF-8");
     mainMenu();
     return 0;
 }
